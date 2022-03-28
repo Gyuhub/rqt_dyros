034a39e35afde75e07c7f453378a800629497501
Gyuhub <Gyuhub@github.com>
2022-03-28 17:03:54 +0900

    add euler angle blocks

--- src/rqt_dyros/husky_panda_widget.py
+++ src/rqt_dyros/husky_panda_widget.py
@@ -1,9 +1,11 @@
 from __future__ import division
-from ast import Str
+#from ast import Str
+#from asyncio import QueueEmpty
 import os
 import rospy
 import rospkg
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
 from std_msgs.msg import String
 
 from python_qt_binding import loadUi
@@ -26,9 +28,13 @@ class HuskyPandaWidget(QMainWindow):
             self.x = 0.0
             self.y = 0.0
             self.z = 0.0
+            self.roll = 0.0
+            self.pitch = 0.0
+            self.yaw = 0.0
 
             # Push Button
             self.open_push_button.pressed.connect(self.open_push_button_pressed)
+            self.stop_push_button.pressed.connect(self.stop_push_button_pressed)
             self.reach_push_button.pressed.connect(self.reach_push_button_pressed)
 
             # Double Spin Box
@@ -38,9 +44,18 @@ class HuskyPandaWidget(QMainWindow):
             self.y_double_spin_box.setMaximum(100.0)
             self.z_double_spin_box.setMinimum(-100.0)
             self.z_double_spin_box.setMaximum(100.0)
+            self.roll_double_spin_box.setMinimum(-100.0)
+            self.roll_double_spin_box.setMaximum(100.0)
+            self.pitch_double_spin_box.setMinimum(-100.0)
+            self.pitch_double_spin_box.setMaximum(100.0)
+            self.yaw_double_spin_box.setMinimum(-100.0)
+            self.yaw_double_spin_box.setMaximum(100.0)
             self.x_double_spin_box.valueChanged.connect(self.x_double_spin_box_changed)
             self.y_double_spin_box.valueChanged.connect(self.y_double_spin_box_changed)
             self.z_double_spin_box.valueChanged.connect(self.z_double_spin_box_changed)
+            self.roll_double_spin_box.valueChanged.connect(self.roll_double_spin_box_changed)
+            self.pitch_double_spin_box.valueChanged.connect(self.pitch_double_spin_box_changed)
+            self.yaw_double_spin_box.valueChanged.connect(self.yaw_double_spin_box_changed)
 
       @Slot()
       def open_push_button_pressed(self):
@@ -51,13 +66,23 @@ class HuskyPandaWidget(QMainWindow):
             self.publisher.publish(msg)
 
       @Slot()
-      def reach_push_button_pressed(self):
-            msg = Vector3()
-            msg.x = self.x
-            msg.y = self.y
-            msg.z = self.z
+      def stop_push_button_pressed(self):
+            msg = String()
+            msg = 'Stop!'
             rospy.loginfo('Publish a message: {0}'.format(msg))
-            self.publisher = rospy.Publisher('/reach_push_button_pressed', Vector3, queue_size=10)
+            self.publisher = rospy.Publisher('/stop_push_button_pressed', String, queue_size=10)
+            self.publisher.publish(msg)
+
+      @Slot()
+      def reach_push_button_pressed(self):
+            msg = Pose()
+            msg.position.x = self.x
+            msg.position.y = self.y
+            msg.position.z = self.z
+            msg.orientation = quaternion_from_euler(self.roll, self.pitch, self.yaw, 'sxyz')
+            
+            rospy.loginfo('Publish a message: {0}'.format(msg))
+            self.publisher = rospy.Publisher('/reach_push_button_pressed', Pose, queue_size=10)
             self.publisher.publish(msg)
 
       def x_double_spin_box_changed(self, value):
@@ -68,6 +93,15 @@ class HuskyPandaWidget(QMainWindow):
       
       def z_double_spin_box_changed(self, value):
             self.z = value
+
+      def roll_double_spin_box_changed(self, value):
+            self.roll = value
+
+      def pitch_double_spin_box_changed(self, value):
+            self.pitch = value
+      
+      def yaw_double_spin_box_changed(self, value):
+            self.yaw = value
 
       def unregister_publisher(self):
             if self.publisher is not None:
